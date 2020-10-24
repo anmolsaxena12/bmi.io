90ea1be1d2562fce23c7b14426d2f2c534e1bbb9
anmolsaxena12 <66118169+anmolsaxena12@users.noreply.github.com>
2020-10-24 22:34:08 +0530

    alert class changes corrected

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
 function bmiCalculator (weight, height) {
     var bmi = weight.value/(height.value*height.value);
     var interpretation;
     if(bmi<18.5){
         interpretation = "Your BMI is "+bmi.toFixed(2)+", so you are underweight.";
-        document.getElementById("result").classList.add("alert-danger");
+        document.getElementById("result").className="alert alert-danger";
     }
     else if(bmi>24.9){
         interpretation = "Your BMI is "+bmi.toFixed(2)+", so you are overweight.";
-        document.getElementById("result").classList.add("alert-danger");
+        document.getElementById("result").className="alert alert-danger";
     }
     else{
         interpretation = "Your BMI is "+ bmi.toFixed(2) +", so you have a normal weight.";
-        document.getElementById("result").classList.add("alert-success");
+        document.getElementById("result").className="alert alert-success";
     }
     document.getElementById("result").innerHTML = interpretation;
 
